472fd41a6ed44760f49a7bf74e2874bc2d827f71
albertshuhar <96998479+albertshuhar@users.noreply.github.com>
2023-02-11 22:09:15 -0500

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,15 @@
+
+# madlibs
+
+print("Hello and welcome to this madlib game! Hope you enjoy it:)")
+
+color = input("Input a color: ")
+
+name = input("Input a name: ")
+country = input("Input a country: ")
+
+
+madlib = f"Hello {name}, why is your face so {color}? Are you from {country}?"
+
+print(madlib)
+
